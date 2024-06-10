df5e26dbf86ce85398d1f0f47e70e800ed6f2127
xan <s.effendi93@gmail.com>
2024-06-10 19:41:06 +0200

    define subnets

--- main.tf
+++ main.tf
@@ -12,6 +12,25 @@ provider "aws" {
 }
 
 # Create Virtual Private Cloud (VPC)
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+}
+
+# Create Subnets
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "af-south-1a"
+}
+
+resource "aws_subnet" "data_subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "af-south-1a"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "af-south-1b"
 }
--- variables.tf
+++ variables.tf
@@ -3,6 +3,6 @@ variable "region" {
   default     = "af-south-1"
   validation {
     condition     = contains(["eu-west-1", "af-south-1"], var.region)
-    error_message = "Please use af-south-1 avoid unwanted costs"
+    error_message = "Please use af-south-1 to avoid unwanted costs"
   }
 }
